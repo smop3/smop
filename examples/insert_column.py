c6d79cb8b5c0c97bce901b350c36ec423ac18605
bo.yang <bo.yang@telecwin.com>
2023-02-17 00:46:15 +0800

    feat[libsmop]: 导入改为 from smop.lib import *

--- examples/insert_column.py
+++ examples/insert_column.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # Generated with SMOP 0.42-beta Improved by 杨波@塔尔旺科技 2023
 try:
-    from smop.lib.libsmop import *
+    from smop.lib import *
 except ImportError:
     raise ImportError('File compiled with `smop3`, please install `smop3` to run it.') from None
 # examples\insert_column.m
